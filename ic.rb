f0b89a081856f6d96e3cebd11983b8a0d75e133b
tomholford <tomholford@users.noreply.github.com>
2020-03-04 18:34:44 +0900

    remove legacy files

--- ic.rb
+++
@@ -1,6 +0,0 @@
-require 'irb'
-require_relative 'labs_api'
-token = File.open(File.expand_path('~/.secrets/twitter/test.token', __dir__)).read.strip
-api = LabsAPI.new(bearer_token: token, debug: true)
-
-binding.irb
