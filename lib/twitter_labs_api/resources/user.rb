5398f3488901129857c0460104b8b1b927d99598
tomholford <tomholford@users.noreply.github.com>
2020-08-26 16:49:08 -0700

    update User resource

--- lib/twitter_labs_api/resources/user.rb
+++ lib/twitter_labs_api/resources/user.rb
@@ -2,44 +2,45 @@ module TwitterLabsAPI
   module Resources
     module User
       DEFAULT_USER_FIELDS = %w[id name username].freeze
+      DEFAULT_FIELDS = { user: DEFAULT_USER_FIELDS }.freeze
 
       # Returns a variety of information about a single more user specified by the requested ID.
       # @param [String] :id the ID of the requested User
-      # @param [Array<String>] :user_fields (["name", "username"]) the list of fields to retrieve for the given User
+      # @param [Hash] :fields  a hash for fields to include in the response payload; 
+      #
+      #  e.g.: { user: %w[id name username] }
       # @return Hash an object with requested user fields
-      def get_user(id:, user_fields: DEFAULT_USER_FIELDS)
+      def get_user(id:, fields: DEFAULT_FIELDS)
         url = "https://api.twitter.com/labs/2/users/#{id}"
-        params = {
-          'user.fields' => user_fields.join(',')
-        }.compact
+        params = ParamsService.from_fields(fields)
 
         make_request(url: url, params: params)
       end
 
       # Returns a variety of information about one or more Users specified by the requested IDs.
       # @param [Array<String>] :ids the collection of requested User IDs
-      # @param [Array<String>] :user_fields (["name", "username"]) the list of fields to retrieve for the given User
+      # @param [Hash] :fields  a hash for fields to include in the response payload; 
+      #
+      #  e.g.: { user: %w[id name username] }
       # @return [Array<Hash>] of user objects with the requested user fields
-      def get_users(ids:, user_fields: DEFAULT_USER_FIELDS)
+      def get_users(ids:, fields: DEFAULT_FIELDS)
         url = 'https://api.twitter.com/labs/2/users'
-        params = {
-          'ids' => ids.join(','),
-          'user.fields' => user_fields.join(',')
-        }.compact
+        params = ParamsService.from_fields(fields)
+        params.merge!({ ids: ids.join(',') })
 
         make_request(url: url, params: params, is_collection: true)
       end
 
       # Returns a variety of information about one or more Users specified by the requested usernames.
       # @param [Array<String>] :usernames the collection of requested Usernames
-      # @param [Array<String>] :user_fields (["name", "username"]) the list of fields to retrieve for the given User
+      # @param [Hash] :fields  a hash for fields to include in the response payload; 
+      #
+      #  e.g.: { user: %w[id name username] }
       # @return [Array<Hash>] of user objects with the requested user fields
-      def get_users_by_usernames(usernames:, user_fields: DEFAULT_USER_FIELDS)
+      def get_users_by_usernames(usernames:, fields: DEFAULT_FIELDS)
         url = 'https://api.twitter.com/labs/2/users/by'
-        params = {
-          'usernames' => usernames.join(','),
-          'user.fields' => user_fields.join(',')
-        }.compact
+        params = ParamsService.from_fields(fields)
+        params.merge!({ usernames: usernames.join(',') })
 
         make_request(url: url, params: params, is_collection: true)
       end
